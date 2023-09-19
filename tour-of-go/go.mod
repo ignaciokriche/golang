0a94a45baa4a0edb68f20d6a0c1f0d7b6a6fb555
ignaciokriche <ignaciokriche@gmail.com>
2023-09-19 10:19:54 -0300

    tour of go.

---
+++ tour-of-go/go.mod
@@ -0,0 +1,3 @@
+module github.com/ignaciokriche/golang
+
+go 1.21.1
---
+++ tour-of-go/hello.go
@@ -0,0 +1,13 @@
+// run go mod init github.com/ignaciokriche/golang
+
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	fmt.Println("hello go!")
+	fmt.Println("the time is: ", time.Now())
+}
