9a3a018f7f995faf19257e3eb80129ae7095d177
ignaciokriche <ignaciokriche@gmail.com>
2023-09-20 16:55:34 -0300

    functions.

--- tour-of-go/functions.go
+++ tour-of-go/functions.go
@@ -24,9 +24,38 @@ func FunctionsDemo() {
 	fmt.Println(compute(hypot))
 	fmt.Println(compute(math.Pow))
 
+	functionClosures()
+
 	fmt.Println()
+
 }
 
 func compute(fn func(float64, float64) float64) float64 {
 	return fn(3, 4)
+}
+
+func functionClosures() {
+
+	fmt.Println("Function closures")
+
+	// Go functions may be closures. A closure is a function value that references variables from outside its body.
+	// The function may access and assign to the referenced variables; in this sense the function is "bound" to
+	// the variables.
+
+	// For example, the adder function returns a closure. Each closure is bound to its own sum variable.
+	pos := adder()
+	for i := 0; i < 10; i++ {
+		fmt.Println(i, pos(i))
+	}
+
+	fmt.Println()
+
+}
+
+func adder() func(int) int {
+	sum := 0
+	return func(x int) int {
+		sum += x
+		return sum
+	}
 }
