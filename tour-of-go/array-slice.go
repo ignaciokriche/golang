da1bcf9394c5a77be80768f64b591ac7b5f0f19e
ignaciokriche <ignaciokriche@gmail.com>
2023-09-20 11:46:22 -0300

    append to a slice.

--- tour-of-go/array-slice.go
+++ tour-of-go/array-slice.go
@@ -14,8 +14,7 @@ func ArraySliceDemo() {
 	fmt.Println()
 
 	numberSlice := numbers[4:9]
-	fmt.Println("number slice:", numberSlice)
-	fmt.Println()
+	printSlice(numberSlice)
 
 	// A slice does not store any data, it just describes a section of an underlying array.
 	// Changing the elements of a slice modifies the corresponding elements of its underlying array.
@@ -33,20 +32,55 @@ func ArraySliceDemo() {
 	// The length and capacity of a slice s can be obtained using the expressions len(s) and cap(s).
 	// You can extend a slice's length by re-slicing it, provided it has sufficient capacity.
 
-	fmt.Printf("length: %v, capacity: %v, slice: %v\n", len(numberSlice), cap(numberSlice), numberSlice)
+	printSlice(numberSlice)
 
 	makeSlices()
 
-	fmt.Println()
+	appendSlice()
 
 }
 
 func makeSlices() {
 
 	s1 := make([]int, 8)
-	fmt.Printf("length: %v, capacity: %v, slice: %v\n", len(s1), cap(s1), s1)
+	printSlice(s1)
 
 	s2 := make([]int, 3, 8)
-	fmt.Printf("length: %v, capacity: %v, slice: %v\n", len(s2), cap(s2), s2)
+	printSlice(s2)
 
+	fmt.Println()
+}
+
+func appendSlice() {
+
+	fmt.Println("appending to a slice:")
+
+	// The resulting value of append is a slice containing all the elements of the original slice plus the provided values.
+	// If the backing array of s is too small to fit all the given values a bigger array will be allocated.
+	// The returned slice will point to the newly allocated array.
+
+	var s []int
+	printSlice(s)
+
+	s = append(s, 1)
+	printSlice(s)
+
+	s = append(s, 2, 3, 4, 5)
+	printSlice(s)
+
+	s = append(s, 6)
+	printSlice(s)
+
+	s = append(s, 7, 8, 9)
+	printSlice(s)
+
+	s = append(s, 10)
+	printSlice(s)
+
+	fmt.Println()
+
+}
+
+func printSlice(s []int) {
+	fmt.Printf("length: %v, capacity: %v, slice: %v\n", len(s), cap(s), s)
 }
