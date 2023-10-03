4f823f6556afe7723bf3887af89f60c6fa923090
ignaciokriche <ignaciokriche@gmail.com>
2023-10-03 15:00:56 -0300

    sum digits.

--- exercises/sum-digits.go
+++ exercises/sum-digits.go
@@ -1,8 +1,37 @@
+/**
+ * You are given a string of digits such as "123456"
+ * your goal is to write a method to sum each digit from left to right and
+ * if the sum has more than 2 digits then you first need to sum those digits
+ * until only one digit is left, and then you keep moving.
+ *
+ * For the example above: "123456"
+ * 1 + 2 + 3 + 4 = 10
+ * 1 + 0 = 1
+ * 1 + 5 = 6
+ * 6 + 6 = 12
+ * 1 + 2 = 3
+ * then sumDigits("123456") = 3
+ *
+ * For: "99"
+ * 9 + 9 = 18
+ * 1 + 8 = 9
+ * then sumDigits("99") = 9
+ *
+ * For: "14522"
+ * 1 + 4 + 5 = 10
+ * 1 + 0 = 1
+ * 1 + 2 = 3
+ * 3 + 2 = 5
+ *
+ * then sumDigits("14522") = 5
+ *
+ * Author: Ignacio Krichevsky
+ *
+ */
+
 package exercises
 
-import (
-	"strconv"
-)
+import "strconv"
 
 func SumDigits(digits string) int {
 
